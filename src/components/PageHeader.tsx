98bb960dc01411ea6fae0fd3c83d24894f03607f
fadeyush <a.fadeyush@gmail.com>
2024-02-13 20:43:17 +0300

    добавление логотипа, реализация выпадающего меню

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import '../styles/PageHeader.scss';
 import { PageHederItemProps } from '../types/pageHeader';
 import PageHeaderItem from './PageHeaderItem';
+import {ReactComponent as Logo} from '../assets/img/logo-mobile.svg';
 
 const PageHeader: FC = () => {
 
@@ -9,9 +10,23 @@ const PageHeader: FC = () => {
         {name:'Главная', link: '/about'},
         {name:'Каталог продукции', link: '/catalog'},
         {name:'Подбор программы', link: '/program'},
-    ]
+    ];
+
+    const toggleModal = () => {
+        const navBar = document.querySelector('.main-nav__list');
+        const navBarWrapper = document.querySelector('.navLogo');
+        navBar?.classList.toggle("main-nav__list--opened");
+        navBarWrapper?.classList.toggle("navLogo--opened");
+    }
+
     return (
         <div>
+            <div className='navLogo'>
+                <div className='navLogo__wrapper'>
+                    <Logo className='navLogo__elem'/>
+                    <button className='navLogo__button' onClick={()=>toggleModal()}/>
+                </div>
+            </div>
             <ul className='main-nav__list'>
                 {pages.map(page =>
                     <PageHeaderItem key={page.link} link={page.link} name={page.name}/>
