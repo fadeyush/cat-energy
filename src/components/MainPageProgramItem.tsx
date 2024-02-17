4134195a6b8003b950592443a3913e0ad2d59ae7
fadeyush <a.fadeyush@gmail.com>
2024-02-17 15:22:04 +0300

    стилизация кнопки в MainProgram

--- src/components/MainPageProgramItem.tsx
+++ src/components/MainPageProgramItem.tsx
@@ -4,14 +4,13 @@ import classes from '../styles/ProgramMainPage.module.scss'
 import { NavLink } from 'react-router-dom';
 import { Pages } from '../types/pageHeader';
 
-
 const MainPageProgramItem: FC<ProgramItemProps> = ({info, catalogName, catalogButtonName, className}) => {
     return (
         <li className={classes.program__item}>
             <div className={classes.program__item__wrapper}>
                 <h3 className={`${classes.program__title } ${classes[className]}`}>{catalogName}</h3>
                 <p className={classes.program__info}>{info}</p>
-                <NavLink to={Pages.catalog} className={classes.program__button}>{catalogButtonName}</NavLink>
+                <NavLink to={Pages.catalog} className={classes.program__button}>{catalogButtonName}<span></span></NavLink>
             </div>
         </li>
     );
