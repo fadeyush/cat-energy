31aeb13fc65e3721592164380f0e5ef09ab580d4
fadeyush <a.fadeyush@gmail.com>
2024-02-21 16:58:47 +0300

    реализован запрос на получение комментариев

---
+++ src/store/action-creator/comments.ts
@@ -0,0 +1,17 @@
+import { Dispatch } from "redux";
+import { CommentsAction, CommentsActionTypes } from "../../types/comments";
+import axios from "axios";
+
+export const fetchComments = () => {
+    return async(dispatch: Dispatch<CommentsAction>) => {
+        try {
+            dispatch({type: CommentsActionTypes.FETCH_COMMENTS});
+
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/comments`)
+            dispatch({type: CommentsActionTypes.FETCH_COMMENTS_SUCCESS, payload: response.data})
+        } catch (e) {
+            console.log(e)
+            dispatch({type: CommentsActionTypes.FETCH_COMMENTS_ERROR, payload: 'Произошла ошибка при загрузке отзывов!'})
+        }
+    }
+}
