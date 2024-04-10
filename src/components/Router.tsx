3a37e38ee3e92c1785d5079f46c4947c391ea61c
fadeyush <a.fadeyush@gmail.com>
2024-04-10 17:20:45 +0300

    сохранение авторизации

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,10 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { privateRouter, publicRouter } from './UI/router';
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useDispatch } from 'react-redux';
 
 const Router: FC = () => {
     const {isAuth} = useTypedSelector(state => state.isAuth);
+    const dispatch = useDispatch();
+    useEffect(() =>{
+        if (localStorage.getItem('auth')) {
+            dispatch({type: 'LOG_IN', payload: true})
+        }
+    }, [])
     return (
         <>{isAuth ?
             <Routes>
--- src/store/reducers/authReducer.ts
+++ src/store/reducers/authReducer.ts
@@ -13,8 +13,10 @@ const inittialState: IsAuthProps = {
 export const isAuthReducer = (state = inittialState, action: IsAuthAction): IsAuthProps => {
     switch (action.type) {
         case 'LOG_IN':
+            localStorage.setItem('auth', 'true'); 
             return {...state, isAuth: true}
         case "LOG_OUT":
+            localStorage.removeItem('auth');
             return {...state, isAuth: false}
         default:
             return state
