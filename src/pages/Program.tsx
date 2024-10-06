48d3c1b1a5f666d10284f2ca256d8e42df3f74a7
fadeyush <a.fadeyush@gmail.com>
2024-10-06 19:19:35 +0300

    определение простых полей

--- src/pages/Program.tsx
+++ src/pages/Program.tsx
@@ -6,7 +6,7 @@ import { buttonTypes } from '../types/button';
 import { ProgrammFormProps } from '../types/programForm';
 
 const Program: FC = () => {
-    const { register, handleSubmit } = useForm<ProgrammFormProps>({
+    const { register, handleSubmit, formState } = useForm<ProgrammFormProps>({
         mode: 'onChange'
     });
 
@@ -14,18 +14,94 @@ const Program: FC = () => {
         console.log(data)
     }
 
-    return (
-        <main className={classes.Program__wrappper}>
-            <h1 className={classes.Program__title}>Подбор программы</h1>
+    const errName = formState.errors['name']?.message;
+    const errWeight = formState.errors['weight']?.message;
+    const errAge = formState.errors['age']?.message;
+    const errEmail = formState.errors['email']?.message
+    const errPhone = formState.errors['phone']?.message
 
-            {/* <form onSubmit={handleSubmit(onSubmit)}>
+    return (
+        <main className={classes.program__wrappper}>
+            <h1 className={classes.program__title}>Подбор программы</h1>
+            <p className={classes.program__info}>Заполните анкету и мы подберем программу питания для вашего кота</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div> 
+                    <label>
+                    Имя:
+                    <input type='text' {...register("name", { 
+                        required: 'Обязательное поле', 
+                    })} placeholder='БАРСИК'></input>
+                    </label>
+                    {errName && <p className={classes.program__error}>{errName}</p>}
+                </div>
+
+                <div>
+                    <label>
+                    Вес (кг):
+                    <input type='text' {...register("weight", { 
+                        required: 'Обязательное поле',
+                        pattern: {
+                        value: /^\s*[\d]+(?:,[\d]+)?\s*$/,
+                        message: "Неправильно введен вес"
+                        }
+                    })} placeholder='7'></input>
+                    </label>
+                    {errWeight && <p className={classes.program__error}>{errWeight}</p>}
+                </div>
+
+                <div>
                 <label>
-                    Имя
-                    <input {...register("name", { required: true})} placeholder='БАРСИК'></input>
-                    <input {...register("age", { required: true , valueAsNumber : true})} placeholder='7'></input>
-                </label>
+                        Возраст (лет):
+                        <input type='text' {...register("age", { 
+                            required: 'Обязательное поле', 
+                            pattern: {
+                                value: /^\s*[\d]+(?:,[\d]+)?\s*$/,
+                                message: "Неправильно введен возраст"
+                            },
+                        })} placeholder='7'></input>
+                    </label>
+                    {errAge && <p className={classes.program__error}>{errAge}</p>}
+                </div>
+
+                <div>
+                    <label>
+                        E-mail:
+                        <input type='email' {...register("email", { 
+                            required: 'Обязательное поле', 
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Неправильно введен email"
+                            },
+                        })} placeholder='kuklachev@gmail.com'></input>
+                    </label>
+                    {errEmail && <p className={classes.program__error}>{errEmail}</p>}
+                </div>
+
+                <div>
+                    <label>
+                        Телефон:
+                        <input type='tel' {...register("phone", {
+                            required: 'Обязательное поле', 
+                            minLength: { value: 11, message: 'Неправильно введен телефон'},
+                            maxLength: { value: 15, message: 'Неправильно введен телефон'},
+                            pattern: {
+                                value: /^\s*[\d]+(?:,[\d]+)?\s*$/,
+                                message: "Неправильно введен телефон"
+                            },
+                        })} placeholder='8 (960) 900-60-90'></input>
+                    </label>
+                    {errPhone && <p className={classes.program__error}>{errPhone}</p>}
+                </div>
+                
+                <div>
+                    <label>
+                        Комментарии:
+                        <textarea {...register("comments")} placeholder='Расскажите обо всех повадках кота'></textarea>
+                    </label>
+                </div>
+
                 <MyButton colorType={buttonTypes.green} type='submit'>Отправить заявку</MyButton>
-            </form> */}
+            </form>
         </main>
     );
 };
